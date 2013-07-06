94ee5cdd896180aba695f7bc9788c78d1cac0ea2
Ubuntu <ubuntu@ip-172-31-17-243.eu-west-1.compute.internal>
2013-07-06 12:03:23 +0000

    Add buffer

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var buffer = new Buffer("I'm a string!", "utf-8");
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
